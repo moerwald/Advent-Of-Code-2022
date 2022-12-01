35001952cb7da4e0dc2ae30b3f16c1993e20dbb9
moerwald <andreas.mewald@gmail.com>
2022-12-01 21:36:28 +0100

    01_12_2022 Added CSharp and FSharp

---
+++ src/01_12/CSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿var caloriesPerElf =
+ File.ReadAllText("./input.txt")
+            .Split("\r\n\r\n")
+            .Select(x => x.Split("\r\n").Select(int.Parse))
+            .Select(x => x.Sum());
+
+Console.WriteLine(caloriesPerElf.Max());
+Console.WriteLine(caloriesPerElf.OrderDescending().Take(3).Sum());
---
+++ src/01_12/FSharp/Program.fs
@@ -0,0 +1,22 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System.IO
+
+let split (pattern: string) (str: string) = str.Split(pattern)
+let splitToElv (text: string) =
+    text
+    |> split "\r\n\r\n"
+    |> Array.map (split "\r\n")
+    |> Array.map (Array.map int)
+    |> Array.map Array.sum
+let elvCalories = splitToElv (File.ReadAllText("input.txt"))
+
+elvCalories
+    |> Array.sortDescending
+    |> Array.max
+    |> printfn "%i"
+    
+elvCalories
+    |> Array.sortDescending
+    |> Array.take 3
+    |> Array.sum
+    |> printfn "%i"
