28083d7819355db1e7c8e62bf04afa15a9029d5a
moerwald <andreas.mewald@gmail.com>
2022-12-08 21:19:37 +0100

    05_12 Solved part 1

--- src/05_12/FSharp/05_12_Tests/Tests.fs
+++ src/05_12/FSharp/05_12_Tests/Tests.fs
@@ -2,31 +2,20 @@ module Tests
 
 open System
 open Xunit
-
-
-
-(*
-
-    - Get stack index line from file and save it's index
-    - Parse the line and find the number of stacks
-    - Parse lines before index line
-        - from line0 to lineBeforeIndexLine
-            - get index for opening bracket [
-            - get content of all opening brackets and store it in
-              the array corresponding the stack index
-
-        - walk through all stack arrays and reverse the array
-
-    -parse move commands line by line
-        -
-*)
-
+open FSharp.Scanf
 
 module AoC =
+    
+    type Move = {
+          _amount: int
+          _from: int
+          _to: int
+        }
+    
     let readLines fileName = System.IO.File.ReadAllLines fileName
 
     let rec getNumberOfStacks index = function
-        | h: string :: _ when h.Contains("1") ->
+        | h:string :: _ when h.Contains("1") ->
             let arr =
                 h.Trim().ToCharArray()
                 |> Array.filter (fun x -> x <> ' ')
@@ -54,32 +43,60 @@ module AoC =
 
             (arrayIdx, snd x))
 
-    let fillStackArray (lines: string list) (array: string[,]) =
+    let fillStackArray (lines: string list) (array: string[]) =
         lines
         |> List.rev
         |> List.map getCratesFromLine
-        |> List.iteri (fun i x ->
+        |> List.iter (fun x ->
             x |> Array.iter (fun y ->
-                let rowIdx = i
-                let columneIdx = (fst y) - 1
-                array[rowIdx, columneIdx] <- string (snd y))
+                let columnIdx = (fst y) - 1
+                let crate = string (snd y)
+                let stack = array[columnIdx]
+                
+                array[columnIdx] <- stack + crate )
             )
         
     let constructCrateArray lines =
-       let rows, columns =
-           lines |> getNumberOfStacks 0
-
-       let array =
-            Array2D.init rows columns (fun _ _ -> " ")
-
+       let rows, columns = lines |> getNumberOfStacks 0
+       let array = Array.init columns (fun _ -> "")
        fillStackArray (lines |> List.take rows) array
        array
-
+       
+    let parseMoves lines =
+        lines
+        |> List.filter (fun (x:string) -> x.Contains "move" )
+        |> List.map (fun x ->
+            let _, a, _, f, _, t =  sscanf "%s %d %s %d %s %d" x
+            // Adapt to array index
+            { _amount = a ; _from = f - 1; _to = t - 1  } )
+        
+    let moveCrate (array:string[]) (move:Move)=
+        let from = move._from
+        let sourceColumn = array[from]
+        let numberOfItemsToKeep = sourceColumn.Length - move._amount
+        let keepNumberOfItems (column:string) nrOfItems = column.Remove nrOfItems
+        
+        let stackToMove = array[from]
+                          |> Seq.skip numberOfItemsToKeep
+                          |> Seq.take move._amount
+                          |> Seq.rev
+                          |> String.Concat
+        
+        array[from] <- keepNumberOfItems sourceColumn numberOfItemsToKeep
+        array[move._to]  <- array[move._to] + stackToMove
+        
     let part1 fileName =
-        let lines =
-            List.ofArray (readLines fileName)
-        constructCrateArray lines
-        failwith "XXX"
+        let lines = List.ofArray (readLines fileName)
+        let crateArray = constructCrateArray lines
+        let moves = parseMoves lines
+        
+        moves
+        |> List.iter (moveCrate crateArray)
+        
+        crateArray
+        |> Seq.map (fun x -> x.Substring(x.Length - 1))
+        |> String.concat ""
+        
 
 open AoC
 open FsUnit
@@ -117,27 +134,35 @@ let ``get crates from line, assert crates`` () =
     |> should equal [ 'L'; 'F'; 'G'; 'C'; 'L'; 'N'; 'N' ]
 
 [<Theory>]
-[<InlineData("test_input.txt", 3,  3)>]
-[<InlineData("input.txt", 8, 9)>]
-let ``constructCrateArray, check returned dimensions`` filename expectedRows expectedColumns =
-    let a = List.ofSeq (readLines filename)
-            |> constructCrateArray
-    Array2D.length1 a |> should equal expectedRows
-    Array2D.length2 a |> should equal expectedColumns
+[<InlineData("test_input.txt",   3)>]
+[<InlineData("input.txt",  9)>]
+let ``constructCrateArray, check returned dimensions`` filename expectedColumns =
+    let a = List.ofSeq (readLines filename) |> constructCrateArray
+    a.Length |> should equal expectedColumns
     
 [<Theory>]
 [<InlineData("test_input.txt")>]
 let ``constructCrateArray, check content`` filename=
-    let (a:string[,]) = List.ofSeq (readLines filename) |> constructCrateArray
+    let (a:string[]) = List.ofSeq (readLines filename) |> constructCrateArray
     
-    a[0,0] |> should equal "Z"
-    a[0,1] |> should equal "M"
-    a[0,2] |> should equal "P"
-            
-    a[1,0] |> should equal "N"
-    a[1,1] |> should equal "C"
-    a[1,2] |> should equal " "
+    a[0] |> should equal "ZN"
+    a[1] |> should equal "MCD"
+    a[2] |> should equal "P"
     
-    a[2,0] |> should equal " "
-    a[2,1] |> should equal "D"
-    a[2,2] |> should equal " "
+[<Theory>]
+[<InlineData("move 1 from 2 to 1", 1, 1 ,0)>]
+[<InlineData("move 3 from 1 to 3", 3, 0 ,2)>]
+[<InlineData("move 2 from 2 to 1", 2, 1, 0)>]
+[<InlineData("move 1 from 1 to 2", 1 ,0 ,1)>]
+let ``parse move list`` line amountExpected fromExpected toExpected =
+    let result = parseMoves [line]
+    result.Head._amount |> should equal amountExpected
+    result.Head._from |> should equal fromExpected
+    result.Head._to |> should equal toExpected
+    
+[<Theory>]
+[<InlineData("test_input.txt", "CMZ")>]
+[<InlineData("input.txt", "QPJPLMNNR")>]
+let ``part, check content`` filename expected  =
+    part1 filename
+    |> should equal expected
