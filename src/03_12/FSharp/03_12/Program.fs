dca912937c7125173e7049cb126d52817595f2c0
moerwald <andreas.mewald@gmail.com>
2022-12-19 22:37:54 +0100

    08_12 Part 1 is working

--- src/03_12/FSharp/03_12/Program.fs
+++ src/03_12/FSharp/03_12/Program.fs
@@ -6,49 +6,100 @@ module Utils =
         match item with
         | item when Char.IsLower(item) -> int item - 96 // subtract the ASCII offset
         | _ -> int item - 38 // subtract the ASCII offset
-        
+
     let loadFile filePath = File.ReadAllLines filePath
 
 module Part1 =
     let splitToCompartments (rucksack: string) =
         let halfIndex = rucksack.Length / 2
-        (Set.ofSeq (rucksack[.. halfIndex - 1]), Set.ofSeq (rucksack[halfIndex  ..]))
+        (Set.ofSeq (rucksack[.. halfIndex - 1]), Set.ofSeq (rucksack[halfIndex..]))
 
     let getDuplicateItemInRucksack rucksack =
-        let compartments = rucksack |> splitToCompartments
-        Set.intersect (fst compartments) (snd compartments) |> Set.maxElement
+        let compartments =
+            rucksack |> splitToCompartments
+
+        Set.intersect (fst compartments) (snd compartments)
+        |> Set.maxElement
 
     let sumOfRucksackItems (items: string []) =
         items
         |> Seq.map getDuplicateItemInRucksack
         |> Seq.map Utils.itemToPriority
         |> Seq.sumBy id
-    
+
 module Part2 =
-    let findBadge (items: string[]) =
+
+    let findBadge (items: string []) =
         items
-        |> Seq.map Set.ofSeq 
-        |> Set.intersectMany
-        |> Set.maxElement
-        |> Utils.itemToPriority
+        |> Seq.splitInto (items.Length / 3)
+        |> Seq.map (fun x ->
+            x
+            |> Seq.map Set.ofSeq
+            |> Set.intersectMany
+            |> Set.maxElement
+            |> Utils.itemToPriority)
 
 // Part 1
 
 open Utils
 open Part1
 open Part2
-        
-["test_input.txt"; "input.txt"]
+
+[ "test_input.txt"; "input.txt" ]
 |> Seq.map loadFile
 |> Seq.map sumOfRucksackItems
 |> Seq.iter (fun x -> printfn $"%i{x}")
 
 // Part 2
-let load = loadFile "input.txt"
-let nrOfLines = load.Length 
-let s = load
-        |> Seq.splitInto (nrOfLines / 3)
-        |> Seq.map findBadge
-        |> Seq.sum
-        
+
+let addBadgeToSum (source: int seq) = Seq.sum source
+
+let s =
+    "input.txt"
+    |> loadFile
+    |> findBadge
+    |> addBadgeToSum
+
 printfn $"%i{s}"
+
+"AB"
+    |> Seq.map (fun x ->
+        printfn $"first pipeline stage: %c{x}"
+        x)
+    |> Seq.map (fun x ->
+        printfn $"second pipeline stage: %c{x}"
+        x)
+    |> Seq.iter (fun x -> printfn $"%c{x}")
+    
+List.ofSeq "AB"
+    |> List.map (fun x ->
+        printfn $"first pipeline stage: %c{x}"
+        x)
+    |> List.map (fun x ->
+        printfn $"second pipeline stage: %c{x}"
+        x)
+    |> List.iter (fun x -> printfn $"%c{x}")
+ 
+
+type Shape =
+  // The value here is the radius.
+    | Circle of float
+  // The value here is the side length.
+    | EquilateralTriangle of double
+  // The value here is the side length.
+    | Square of double
+  // The values here are the height and width.
+    | Rectangle of double * double
+
+let area  =
+    function
+    | Circle radius ->  3.14 * (radius**2)
+    | Square side -> side * side
+    | Rectangle(h, b) -> h * b
+    | EquilateralTriangle s -> (sqrt 3.0) / 4.0 * s * s
+ 
+let c = Circle 2.
+let areaCircle = area c
+ 
+    
+
--- src/08_12/FSharp/08_12_Tests/Tests.fs
+++ src/08_12/FSharp/08_12_Tests/Tests.fs
@@ -2,7 +2,48 @@ module Tests
 
 open System
 open Xunit
+open FsUnit.Xunit
+
+let createArrayFrom (input: string array) =
+    let a = input
+            |> Array.map (fun x -> x |> Array.ofSeq)
+            |> Array.map (fun x -> x |> Array.map (string >> int))
+    Array2D.init a.Length a.Length (fun i j -> a[i][j] )
+
+let isVisible (treeMap: int[,]) row column =
+    let value = treeMap[row, column]
+
+    let visibleSubArray arr actValue =
+        arr |> Seq.filter (fun x -> x >= actValue) |> Seq.length = 0
+
+    let visible =
+        visibleSubArray treeMap[0 .. row - 1, column] value
+        || visibleSubArray treeMap[(row + 1) .., column] value
+        || visibleSubArray treeMap[row, .. column - 1] value
+        || visibleSubArray treeMap[row, (column + 1) ..] value
+
+    if visible then 1 else 0
+
+let countVisibleTrees treeMap numberOfColumns numberOfRows =
+    seq {
+        for r = 0 to numberOfRows - 1 do
+            for c = 0 to numberOfColumns - 1 do
+                yield isVisible treeMap r c
+    }
+
+let part1 input =
+    let (treeMap: int[,]) = createArrayFrom input
+    let columns = Array2D.length1 treeMap
+    let rows = Array2D.length2 treeMap
+
+    countVisibleTrees treeMap columns rows |> Seq.sum
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``Part1, test input`` () =
+    let input = System.IO.File.ReadAllLines "test_input.txt"
+    part1 input |> should equal 21
+
+[<Fact>]
+let ``Part1, input`` () =
+    let input = System.IO.File.ReadAllLines "input.txt"
+    part1 input |> should equal 1818
